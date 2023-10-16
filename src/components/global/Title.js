8205b2200688c68f1028a0197dbb6e4d280ff9c8
darshan <darshan.karmaln@gmail.com>
2023-10-16 15:18:38 +0530

    changes done

--- src/components/global/Title.js
+++
@@ -1,7 +0,0 @@
-import parse from "html-react-parser";
-
-const Title = ({ title, className }) => {
-  return <h2 className={className}>{parse(`${title}`)}</h2>;
-};
-
-export default Title;
--- src/components/howWePublish/ArticleSubmissionStep.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ArticleSubmissionStep = () => {
-	return <div>ArticleSubmissionStep</div>;
-};
-
-export default ArticleSubmissionStep;
